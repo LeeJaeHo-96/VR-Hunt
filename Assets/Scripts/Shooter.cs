34cc4f1be3f475f9b230257f1db55e09bacbb685
LeeJaeHo-96 <towpam123@gmail.com>
2024-10-20 21:02:43 +0900

    Sound Issue(dont`clear)

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -8,15 +8,62 @@ public class Shooter : MonoBehaviour
     [SerializeField] Transform muzzlePoint;
 
     [SerializeField] float bulletSpeed;
+    [SerializeField] bool isReload;
+    [SerializeField] float reloadingTime = 2;
 
+    [SerializeField] AudioSource bangSound;
+    [SerializeField] AudioSource noBangSound;
 
-    
+    public void Start()
+    {
+        isReload = true;
+    }
     public void Shoot()
     {
-        
+        if (isReload == true)
+        {
             GameObject bullet = Instantiate(bulletPrefab, muzzlePoint.position, muzzlePoint.rotation);
             Rigidbody rigid = bullet.GetComponent<Rigidbody>();
             rigid.velocity = bullet.transform.forward * bulletSpeed;
-        
+
+            isReload = false;
+        }
+        if (isReload == false)
+        {
+            Coroutine reloadCo = StartCoroutine(ReloadCo());
+        }
+    }
+
+    public void Bang()
+    {
+        if (isReload)
+        ShootSound(bangSound);
+    }
+
+    public void NoBang()
+    {
+        if (!isReload)
+            ShootSound(noBangSound);
+    }
+    
+
+   // public void Reload()
+   // {
+   //     if (!isReload)
+   //     {
+   //         Coroutine reloadCo = StartCoroutine(ReloadCo());
+   //     }
+   // }
+
+    IEnumerator ReloadCo()
+    {
+        yield return new WaitForSeconds(reloadingTime);
+        isReload = true;
+        StopAllCoroutines();
+    }
+
+    public void ShootSound(AudioSource sound)
+    {
+        sound.Play();
     }
 }
